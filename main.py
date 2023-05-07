7f58bf3f735985df958201ce87b81580eea90768
darynarodicheva <daryna.r@gowombat.team>
2023-05-07 20:37:10 +0300

    Add code with fixed codestyle

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-from random import randint
+import random
 
-NUM_TRIALS = 10000
-SWITCH_WINS = 0
-STAY_WINS = 0
+num_trials = 10000
+switch_wins = 0
+stay_wins = 0
 
-for x in range(0, NUM_TRIALS):
-    # Start the game with 3 doors, 1 car, 2 goats
-    doors = ["goat", "goat", "car"]
-    randint(0, 2)
+for i in range(num_trials):
+    # Set up the game
+    doors = ['goat', 'goat', 'car']
+    random.shuffle(doors)
 
-    # Player makes a choice
-    player_choice = randint(0, 2)
-    print(f"Player chose door {player_choice}")
+    # Player chooses a door
+    player_choice = random.randint(0, 2)
 
-    # Host reveals one goat
-    for i in range(len(doors)):
-        if i != player_choice and doors[i] == "goat":
-            host_choice = i
-    # doors.pop(host_choice)
-    print(f"Host reveals door {host_choice}")
-
-    # Player chooses whether to switch or stay
-    switch = randint(0, 1)
-    if switch == 1:
-        for i in range(len(doors)):
-            if i != player_choice:
-                player_choice = i
-
-    # Check if the player won
-    print(f"{player_choice=}, {len(doors)=}")
-    if doors[player_choice] == "car":
-        SWITCH_WINS += 1
+    # Host opens a door with a goat
+    if doors[player_choice] == 'car':
+        goat_doors = [i for i in range(3) if i != player_choice]
+        host_choice = random.choice(goat_doors)
     else:
-        STAY_WINS += 1
+        host_choice = [i for i in range(3) if doors[i] != 'car' and i != player_choice][0]
+
+    # Switch doors
+    switch_choice = [i for i in range(3) if i not in [player_choice, host_choice]][0]
+
+    # Determine the outcome
+    if doors[player_choice] == 'car':
+        stay_wins += 1
+    elif doors[switch_choice] == 'car':
+        switch_wins += 1
 
 # Print the results
-print("Percentage of times player won by switching:", SWITCH_WINS/NUM_TRIALS*100)
-print("Percentage of times player won by sticking with original choice:", STAY_WINS/NUM_TRIALS*100)
+print(f"Switching doors won {switch_wins} out of {num_trials} times ({switch_wins/num_trials*100:.2f}%).")
+print(f"Sticking with the original choice won {stay_wins} out of {num_trials} times ({stay_wins/num_trials*100:.2f}%).")
